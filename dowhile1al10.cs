34d807c7f570840f1b865bb9154dd9a1f8dc6847
albertobolio <50672781+albertobolio@users.noreply.github.com>
2019-11-15 01:40:51 -0600

    Add files via upload

---
+++ dowhile1al10.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace selectivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int x = 0;
+            do
+            {
+                x = x + 1;
+                Console.WriteLine(" " + x);
+
+            } while (x != 10);
+
+            Console.WriteLine("Oprime una tecla para continuar "); Console.ReadKey();
+
+
+        }
+    }
+}
