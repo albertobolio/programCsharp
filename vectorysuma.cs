251337242f0f6aa656fca9ee32a209fc58cbda21
albertobolio <50672781+albertobolio@users.noreply.github.com>
2019-11-15 01:22:02 -0600

    Add files via upload

---
+++ vectorysuma.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace vectorysuma
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+
+			int s = 0, i;
+
+			//Arreglo llamado vector
+
+			int[] n = new int[5];
+
+			for (i = 0; i <= 4; i = i + 1)
+			{
+				Console.Write("Introduce el valor: ");
+				n[i] = int.Parse(Console.ReadLine());
+
+			}
+
+			for (i = 0; i <= 4; i = i + 1)
+
+				s = s + n[i];
+
+
+			Console.WriteLine("La suma del vector de 5 valores es = "+ s);
+
+
+			Console.ReadKey();
+
+
+
+		}
+	}
+}
